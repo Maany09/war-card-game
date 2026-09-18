5e1c1aabe7282c3bb6dfe23072cc8f269c1814d6
Maany <maanykansara@gmail.com>
2026-09-18 16:35:08 +0530

    Added player class, with taking name of player, adding cards to its inventory, removing one card from it

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ class Card:
     def __str__(self):
         return self.rank + ' of ' + self.suit
 
-
+two_hearts = Card(suits[0],ranks[0])
 
 # Deck class
 class Deck:
@@ -42,11 +42,46 @@ class Deck:
 
     # Remove and return one card from the deck
     def grab_one(self):
-
         return self.all_cards.pop()
 
 my_deck = Deck()
 my_deck.shuffle()
 
+# grab one card...
 my_card = my_deck.grab_one()
-print(my_card)
+
+
+# Creating a Player class with game logic
+class Player:
+
+    def __init__(self,name):
+        # takes name from the user
+        self.name = name
+
+        # at first, user will have 0 cards
+        self.all_cards = []
+
+    def add_cards(self,mycards):
+        # add cards to the Player.
+        if isinstance(mycards,list):
+            self.all_cards.extend(mycards)
+        else:
+            self.all_cards.append(mycards)
+
+    def remove_one(self):
+        # it will remove the card from the top. so pop(0)
+        try:
+            return self.all_cards.pop(0)
+        except IndexError:
+            print("No cards left!")
+        
+    def __str__(self):
+        return f"{self.name} has {len(self.all_cards)} cards."
+
+name = Player("Alex")
+name.add_cards(my_card)
+name.add_cards([my_card,my_card,my_card])
+print(name)
+print("removing one card")
+name.remove_one()
+print(name)
